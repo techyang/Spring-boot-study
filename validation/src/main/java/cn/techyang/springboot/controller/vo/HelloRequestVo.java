443c971341802ae60ff2d8ec5e2441109a96b1fc
yangwenlong <tecyang@gmail.com>
2023-07-06 21:57:22 +0800

    add comments

--- validation/src/main/java/cn/techyang/springboot/controller/vo/HelloRequestVo.java
+++ validation/src/main/java/cn/techyang/springboot/controller/vo/HelloRequestVo.java
@@ -7,7 +7,7 @@ import javax.validation.constraints.NotBlank;
 /**
  * @author tecyang@gmail.com
  * @version V1.0
- * @title
+ * @title Hello Word 请求类
  * @description
  * @date 2023-06-10 18:33
  */
