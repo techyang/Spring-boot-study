dca8c2ba516ffba1a41499e874dff359282d28cb
yangwenlong <tecyang@gmail.com>
2023-07-08 20:51:54 +0800

    add comments

--- validation/src/main/java/cn/techyang/springboot/controller/DemoController.java
+++ validation/src/main/java/cn/techyang/springboot/controller/DemoController.java
@@ -1,6 +1,7 @@
 package cn.techyang.springboot.controller;
 
 import cn.techyang.springboot.controller.vo.HelloRequestVo;
+import cn.techyang.springboot.controller.vo.HelloResponseVo;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,5 +21,11 @@ public class DemoController {
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello(@RequestBody @Validated  HelloRequestVo helloRequestVo) {
         return "hello world!" + helloRequestVo.getUserName();
+    }
+
+
+    @RequestMapping(value = "/hello2", method = RequestMethod.POST)
+    public HelloResponseVo hello2(@RequestBody @Validated  HelloRequestVo helloRequestVo ){
+        return HelloResponseVo.builder().age(10).name("张三").build();
     }
 }
